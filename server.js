321ec1356be5451a9bffe1ddc25f1e504af625a6
cbrizzy003@gmail.com <cbrizzy003@gmail.com>
2023-10-24 19:37:53 -0500

    sending index and show data

--- server.js
+++ server.js
@@ -6,15 +6,24 @@ const app = express()
 const breadsController = require('./controllers/breads_controllers.js')
 
 // MIDDLEWARE
+app.use(express.static('public'))
+
+// MIDDLEWARE
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
-app.get ('/', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads!')
 })
 
 app.use('/breads', breadsController)
+
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404 BRO!')
+})
+
 
 app.listen(PORT, () => {
     console.log('listening on PORT: ', PORT)
